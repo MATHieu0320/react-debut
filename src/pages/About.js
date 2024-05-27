69de618046b348104a8652d6b9b8a45e3485e411
Mathie <math039@outlook.fr>
2024-05-27 19:55:34 +0200

    composent

---
+++ src/pages/About.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque a
+        repudiandae qui, odit voluptate consectetur aspernatur dolore voluptas
+        facere aperiam accusantium hic earum fuga explicabo dolor corrupti enim.
+        Possimus in deleniti necessitatibus veritatis illo asperiores saepe
+        illum nesciunt consequatur dolore dolor officia magni, consectetur
+        laboriosam? Quae ea eveniet dolorem libero debitis nesciunt nobis
+        mollitia harum earum, labore recusandae consequatur optio reiciendis
+        illum distinctio quisquam doloremque delectus cumque modi at velit
+        repudiandae iure. Iste quam aliquam voluptatem repellat, ratione quae
+        voluptas fuga itaque, laboriosam, eaque pariatur ipsa aliquid
+        repellendus eos vel cumque explicabo veniam eligendi magni? Placeat
+        impedit itaque sequi porro alias obcaecati cumque numquam ratione
+        laborum debitis unde, ex, explicabo voluptatibus modi. Ipsa sed a aut
+        ullam? Harum ex mollitia sapiente, quis aliquid illo nesciunt, at
+        tempora inventore cupiditate, ad suscipit. Alias corrupti eius
+        repellendus vel facere impedit voluptate doloremque rem quasi! Eveniet,
+        numquam. Pariatur repellendus saepe suscipit ducimus aperiam, libero
+        accusamus voluptatum dolorum asperiores porro, necessitatibus error
+        nisi! Nesciunt laborum numquam rerum officiis consequatur, ducimus
+        aspernatur itaque nobis quam molestias tempore perferendis ex. Rem
+        maiores laboriosam unde fugit reiciendis ab voluptatibus voluptas
+        temporibus. Numquam maiores similique repellat sequi iste. Dolor fugit
+        doloremque laboriosam quae quidem ratione et enim veritatis doloribus
+        aliquid maxime accusamus minima exercitationem recusandae voluptatem
+        pariatur, nostrum inventore voluptas molestias unde excepturi eius illo.
+        Obcaecati animi quae omnis culpa iure expedita repellat laborum,
+        consequuntur dolorum reiciendis, perferendis numquam pariatur soluta,
+        esse vel quod. Blanditiis, quos aspernatur? Nostrum, odio. Dolore rem
+        sit necessitatibus sed ea veritatis voluptates minima, illo,
+        perferendis, minus laborum ipsa fugit odit nulla aspernatur consequuntur
+        alias facilis natus corrupti. Corporis minus blanditiis atque id,
+        reiciendis necessitatibus provident inventore non dolores, illo
+        consequatur porro? Sapiente, hic laboriosam perspiciatis excepturi a,
+        error magnam quidem ad magni, fuga aspernatur ipsum. Amet saepe quidem
+        ducimus quibusdam? Voluptatem mollitia, ipsam commodi dolor accusantium
+        minima deleniti, iste tempore esse rerum sit eaque sed dolores aut
+        eveniet illo dolorem, nostrum nobis voluptatum enim facilis. In, quaerat
+        ut architecto mollitia nulla accusamus repudiandae eum facilis pariatur?
+        Rem velit quaerat aliquam aspernatur corporis molestias dolores veniam
+        impedit, ea culpa, voluptate suscipit quis excepturi vitae, dolore ut
+        doloribus. Ab error in magni magnam quaerat. Suscipit eligendi at
+        nesciunt, sunt, vel id expedita laudantium mollitia eos deserunt libero,
+        aperiam perferendis soluta quas! Similique libero facilis mollitia optio
+        earum cupiditate, quidem quas quibusdam consectetur, quo ipsam excepturi
+        dolore ullam assumenda hic quam placeat eius aut nisi aliquam?
+      </p>
+    </div>
+  );
+};
+
+export default About;
