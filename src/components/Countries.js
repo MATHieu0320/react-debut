0749e37f1b0e3416244aa9ed6943d6fb6854b759
Mathie <math039@outlook.fr>
2024-06-03 19:42:11 +0200

    key

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -3,15 +3,22 @@ import React, { useEffect, useState } from "react";
 
 const Countries = () => {
   const [data, setData] = useState([]);
+  const [hello, setHello] = useState(["hello comment cv"]);
   // useffect se joue lorsque le composant est monte appelle
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
-      .then((res) => console.log(res.data));
+      .then((res) => setData(res.data));
   }, []);
   return (
     <div className="countries">
       <h1>countries</h1>
+      <h4> {hello} </h4>
+      <ul>
+        {data.map((country, index) => (
+          <li key={country}>{country.translations.fra.common}</li>
+        ))}
+      </ul>
     </div>
   );
 };
