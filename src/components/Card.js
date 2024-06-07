363ad69e1a4f237a589927926cdb202901e3d2ef
Mathie <math039@outlook.fr>
2024-06-07 19:35:56 +0200

    range

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Card = (propos) => {
-  console.log(propos);
+const Card = ({ country }) => {
   return (
     <li className="card">
+      <img src={country.flags.svg} alt={country.translations.fra.common} />
       <div className="infos">
-        <h2>nom du pays</h2>
+        <h2> {country.translations.fra.common} </h2>
+        <h4> {country.capital} </h4>
+        <p>pop : {country.population.toLocaleString()} </p>
       </div>
     </li>
   );
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,6 +4,8 @@ import Card from "./Card";
 
 const Countries = () => {
   const [data, setData] = useState([]);
+  const [Range, setRange] = useState([36]);
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
   const [hello, setHello] = useState(["hello comment cv"]);
   // useffect se joue lorsque le composant est monte appelle
   useEffect(() => {
@@ -13,11 +15,22 @@ const Countries = () => {
   }, []);
   return (
     <div className="countries">
+      <ul className="radia-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={Range}
+          onChange={(e) => setRange(e.target.value)}
+        />
+      </ul>
+      <input type="radio" />
       <h1>countries</h1>
       <h4> {hello} </h4>
       <ul>
-        {data.map((country, index) => (
+        {data.slice(0, Range).map((country, index) => (
           <Card key={index} country={country} />
+
           // <li key={index}>{country.translations.fra.common}</li>
         ))}
       </ul>
